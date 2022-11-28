9a6714524ba3b113b11e1e3fe4f6f94d7d0a7601
DannyKaschub <dannykashub98@gmail.com>
2022-11-28 21:06:44 +0100

    steuerung fürs handy fertig stellen

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -6,4 +6,65 @@ class Keybord {
     SPACE = false;
     D = false;
     m = false;
+
+
+    constructor() {
+        this.TouchEvents();
+    }
+
+    TouchEvents() {
+        this.btnleft();
+        this.rightTouch();
+        this.jumpTouch();
+        this.throwTouch();
+    }
+
+    btnleft() {
+        document.getElementById('btnleft').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            LEFT = true;
+        });
+
+        document.getElementById('btnleft').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            LEFT = false;
+        });
+    }
+
+    rightTouch() {
+        document.getElementById('btnright').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            keyboard.RIGHT = true;
+        });
+
+        document.getElementById('btnright').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            keyboard.RIGHT = false;
+        });
+    }
+
+    jumpTouch() {
+        document.getElementById('btnjump').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            keyboard.SPACE = true;
+        });
+
+        document.getElementById('btnjump').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            keyboard.SPACE = false;
+        });
+    }
+
+    throwTouchButtons() {
+        document.getElementById('btnthrow').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            keyboard.D = true;
+        });
+
+        document.getElementById('btnthrow').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            keyboard.D = false;
+        });
+    }
+
 }
