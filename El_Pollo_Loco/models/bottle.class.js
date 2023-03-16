7931b9baf70dcc9cb8a6cd4c71d285484d3e1620
DannyKaschub <dannykashub98@gmail.com>
2023-03-16 21:13:32 +0100

    unterschiedliche endscreens gebaut für win/lost

--- El_Pollo_Loco/models/bottle.class.js
+++
@@ -1,8 +0,0 @@
-class Bottle extends MovableObject {
-
-    constructor (x, y) {
-        super().loadImage('img_pollo_locco/img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
-        this.x = y;
-        this.y = x;
-    }
-}
--- El_Pollo_Loco/models/keyboard.class.js
+++
@@ -1,9 +0,0 @@
-class Keybord {
-    LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    D = false;
-    m = false;
-}
